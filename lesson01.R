f17a45ccb3a1aac94d651953a71af7ac74b8a3c8
Kirill Müller <krlmlr+r@mailbox.org>
2019-09-19 16:38:31 +0200

    Add bonus

--- lesson01.R
+++ lesson01.R
@@ -10,3 +10,7 @@
 #' 2. Basic Plotting
 
 # Placeholder: Add R code here
+
+#' 3. Bonus: Read about "GitHub Pages". Create a new directory `docs/`,
+#'    save the 'knitted' result of your work in this directory as `index.html`
+#'    and publish it so that others can see your work in a browser.
