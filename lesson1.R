316489f0ef4a8100fcef6c189641dc7bed51502d
Christoph <christoph@cynkra.com>
2019-09-19 10:58:33 +0200

    Minimal homework

---
+++ lesson1.R
@@ -0,0 +1,12 @@
+#' ## Assignment Set 1: Lesson 1
+#'
+#' Add R Code under the questions. If you are done, 'knit' the document, commit
+#' and push to GitHub.
+#'
+#' 1. Using R as a calculator
+
+# Placeholder: Add R code here
+
+#' 2. Basic Plotting
+
+# Placeholder: Add R code here
