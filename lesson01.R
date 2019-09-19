c84e35e2a20655448fa9f195c5c77f793df90110
Kirill Müller <krlmlr+r@mailbox.org>
2019-09-19 19:32:36 +0200

    Live coding

--- lesson01.R
+++ lesson01.R
@@ -5,11 +5,33 @@
 #'
 #' 1. Using R as a calculator
 
+1 + 2
+
+3 + 5 * 6
+
+3 * 5 +
+  6
+
+3 * 5
++ 6
+
+( (3 + 5) * 6 )
+
 # Placeholder: Add R code here
 
 #' 2. Basic Plotting
 
+# install.packages("tidyverse")
+library(tidyverse)
+
 # Placeholder: Add R code here
+
+mpg
+
+view(mpg)
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point()
 
 #' 3. Bonus: Read about "GitHub Pages". Create a new directory `docs/`,
 #'    save the 'knitted' result of your work in this directory as `index.html`
